6778af6c9e9a432118799393b1e5668251169980
TWCKaijin <kaijinkaijin2@gmail.com>
2026-05-30 06:37:24 +0800

    fix: firebase sdk package

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from bot!")
+from backend.app.main import app
+from firebase_functions.options import HttpsOptions
 
+# For Firebase CLI to detect the ASGI application as a Firebase Function
+app.__firebase_endpoint__ = HttpsOptions(region="asia-east1")._endpoint(func_name="api")
 
-if __name__ == "__main__":
-    main()
+api = app
