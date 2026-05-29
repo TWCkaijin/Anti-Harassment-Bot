c33a571d55ea7172421c5c6d0933cea45e0f90b1
TWCKaijin <kaijinkaijin2@gmail.com>
2026-05-30 07:04:33 +0800

    update: update cred for firebase cli auth

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from backend.app.main import app
 from firebase_functions.options import HttpsOptions
 
 # For Firebase CLI to detect the ASGI application as a Firebase Function
-app.__firebase_endpoint__ = HttpsOptions(region="asia-east1")._endpoint(func_name="api")
+# We wrap the FastAPI app in a standard ASGI function so `inspect.isfunction` evaluates to True.
+async def api(scope, receive, send):
+    await app(scope, receive, send)
 
-api = app
+api.__firebase_endpoint__ = HttpsOptions(region="asia-east1")._endpoint(func_name="api")
